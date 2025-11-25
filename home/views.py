c5828b3da1db5ffaee8da157791b04299c52cd2a
gehsso <jepher_son@hotmail.com>
2025-11-25 14:02:14 -0300

    teste

--- home/views.py
+++ home/views.py
@@ -7,4 +7,4 @@ def index(request):
     return HttpResponse('<h1>Página Principal</h1>')
 
 def sobre(request):
-    return HttpResponse('Sobre o Sistema Django')
+    return HttpResponse('Sobre o Sistema Django!')
