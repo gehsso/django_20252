424bdb145e52e00c10fc3e1861f9e8f80345cd27
gehsso <jepher_son@hotmail.com>
2025-11-28 20:44:42 -0300

    vercel

--- home/tests.py
+++ home/tests.py
@@ -1,3 +1,41 @@
 from django.test import TestCase
 
-# Create your tests here.
+class Conta:
+    def __init__(self,cliente,saldo):
+        self.cliente = cliente
+        self.saldo = saldo
+
+    def depositar(self, valor):
+        if valor > 0:
+            self.saldo += valor
+            return True
+        return False
+
+    def sacar(self, valor):
+        if 0 < valor <= self.saldo:
+            self.saldo -= valor
+            return True
+        return False
+  
+conta_a = Conta('João',100)
+
+print(conta_a.cliente);
+conta_a.depositar(200)
+print(conta_a.saldo)  # 300
+conta_a.sacar(300)
+print(conta_a.saldo)  # 0
+
+
+print(conta_a.cliente);
+
+print(f'CLIENTE A: {conta_a.cliente}');
+
+
+class Aluno:
+    pass
+
+a1 = Aluno('888', 'José')
+a1.adcionar_nota(7)
+a1.adcionar_nota(8)
+a1.adcionar_nota(9)
+print(f'Aluno {a1.nome} - Média: {a1.media()}')
--- home/views.py
+++ home/views.py
@@ -4,7 +4,13 @@ from django.shortcuts import render, HttpResponse
 
 
 def index(request):
-    return HttpResponse('<h1>Página Principal</h1>')
+    return HttpResponse('<h1>Página Principal Novo Teste</h1>')
 
 def sobre(request):
     return HttpResponse('Sobre o Sistema Django!')
+
+def contato(request):
+    return HttpResponse('Contato')
+
+def ajuda(request):
+    return HttpResponse('ajuda')
